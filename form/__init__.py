1dbf215261101c7ef7099c83d4af578e94f71d2c
Jace Browning <jacebrowning@gmail.com>
2018-07-13 01:26:33 -0400

    Implement decorator

--- form/__init__.py
+++ form/__init__.py
@@ -2,44 +2,10 @@ __version__ = "0.1.0"
 
 import dataclasses
 import inspect
+from string import Formatter
 from typing import Any, Dict, Optional
 
 import log
 
 from . import fields
-
-
-class Manager:
-    def __init__(
-        self,
-        synchronized_object: Any,
-        path_pattern: Optional[str],
-        mapped_fields: Dict,
-    ):
-        self._object = synchronized_object
-        self._pattern = path_pattern
-        self.fields = mapped_fields
-
-    @property
-    def path(self) -> Optional[str]:
-        if not self._pattern:
-            return None
-        log.debug(f"Formatting path {self._pattern!r} using {self._object!r}")
-        return self._pattern.format(self=self._object)
-
-    @property
-    def data(self) -> Dict:
-        data = dataclasses.asdict(self._object)
-        for key in list(data.keys()):
-            if key not in self.fields:
-                data.pop(key)
-        return data
-
-
-class Model:
-    @property
-    def form(self) -> Manager:
-        meta = getattr(self, "Meta", None)
-        path = getattr(meta, "form_path", None)
-        fields = getattr(meta, "form_fields", {})
-        return Manager(self, path, fields)
+from .decorators import sync
---
+++ tests/test_sync.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+import pytest
+
+import form
+
+
+@pytest.fixture
+def sample():
+    """A properly-decorated dataclass with a single key."""
+
+    @form.sync('tmp/{self.key}.yml')
+    @dataclass
+    class Sample:
+        key: int
+        name: str
+
+    return Sample(42, "foobar")
+
+
+def describe_sync():
+    def it_populates_metadata(expect, sample):
+        expect(sample.form.fields) == {"name": form.fields.String}
+        expect(sample.form.path) == "tmp/42.yml"
+        expect(sample.form.data) == {"name": "foobar"}
+
+    def it_requires_dataclasses(expect):
+        with expect.raises(ValueError):
+
+            @dataclass
+            @form.sync('tmp/{self.key}.yml')
+            class Sample:
+                key: int
+                name: str
