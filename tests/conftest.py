4dabee967b216a8f5611c10d112007d71c5bdd9c
Jace Browning <jacebrowning@gmail.com>
2018-09-09 21:25:26 -0400

    Run tests randomly

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,21 @@ import pytest
 from . import samples
 
 
+def pytest_configure(config):
+    terminal = config.pluginmanager.getplugin('terminal')
+
+    class QuietReporter(terminal.TerminalReporter):  # type: ignore
+        """Reporter that only shows dots when running tests."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.verbosity = 0
+            self.showlongtestinfo = False
+            self.showfspath = False
+
+    terminal.TerminalReporter = QuietReporter
+
+
 @pytest.fixture(autouse=True)
 def create_tmp():
     path = Path('tmp')
