96ac15f269195de5a898c3c4456c71d485ca1c72
Jace Browning <jacebrowning@gmail.com>
2018-09-10 07:16:52 -0400

    Preserve spacing and comments

--- datafiles/utils.py
+++ datafiles/utils.py
@@ -1,4 +1,24 @@
+from contextlib import suppress
 from functools import lru_cache
+from pprint import pformat
+from typing import Any, Dict
 
 
 cached = lru_cache()
+
+
+def prettify(data: Dict) -> str:
+    return pformat(dictify(data))
+
+
+def dictify(value: Any) -> Dict:
+    with suppress(AttributeError):
+        return {k: dictify(v) for k, v in value.items()}
+
+    if isinstance(value, str):
+        return value
+
+    with suppress(TypeError):
+        return [dictify(x) for x in value]
+
+    return value
