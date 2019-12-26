3d25320b0e0b71db8ee381cef913ee37846543c3
Jace Browning <jacebrowning@gmail.com>
2019-12-26 11:32:11 -0500

    Fix debugger to work with pytest watch

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,1 @@
 """Integration tests for the package."""
-
-# pylint: disable=unused-import
-
-try:
-    from IPython.terminal.debugger import TerminalPdb as Debugger
-except ImportError:
-    from pdb import Pdb as Debugger
