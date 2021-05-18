c9d1ff8f8b22039a79ceee705019ec6a872de8f7
Tomas Babej <tomas@tbabej.com>
2021-05-18 08:13:34 -0400

    tests: Add a test for filtering for a nested value

--- datafiles/tests/test_manager.py
+++ datafiles/tests/test_manager.py
@@ -1,5 +1,6 @@
 # pylint: disable=unused-variable
 
+import typing
 from dataclasses import dataclass
 from unittest.mock import patch
 
@@ -10,9 +11,15 @@ from datafiles.model import create_model
 
 
 @dataclass
+class Nested:
+    name: str
+
+
+@dataclass
 class MyClass:
     foo: int
     bar: int
+    nested: typing.Optional[Nested] = None
 
 
 def describe_manager():
@@ -26,12 +33,14 @@ def describe_manager():
         @patch('datafiles.mapper.Mapper.exists', True)
         @patch('datafiles.mapper.Mapper.modified', False)
         def when_file_exists(mock_load, expect, manager):
-            expect(manager.get_or_none(foo=1, bar=2)) == MyClass(foo=1, bar=2)
+            expect(manager.get_or_none(foo=1, bar=2, nested=None)) == MyClass(
+                foo=1, bar=2
+            )
             expect(mock_load.called).is_(True)
 
         @patch('datafiles.mapper.Mapper.exists', False)
         def when_file_missing(expect, manager):
-            expect(manager.get_or_none(foo=3, bar=4)).is_(None)
+            expect(manager.get_or_none(foo=3, bar=4, nested=None)).is_(None)
 
     def describe_get_or_create():
         @patch('datafiles.mapper.Mapper.save')
@@ -39,7 +48,9 @@ def describe_manager():
         @patch('datafiles.mapper.Mapper.exists', True)
         @patch('datafiles.mapper.Mapper.modified', False)
         def when_file_exists(mock_save, mock_load, expect, manager):
-            expect(manager.get_or_create(foo=1, bar=2)) == MyClass(foo=1, bar=2)
+            expect(manager.get_or_create(foo=1, bar=2, nested=None)) == MyClass(
+                foo=1, bar=2
+            )
             expect(mock_save.called).is_(True)
             expect(mock_load.called).is_(False)
 
@@ -47,7 +58,9 @@ def describe_manager():
         @patch('datafiles.mapper.Mapper.load')
         @patch('datafiles.mapper.Mapper.exists', False)
         def when_file_missing(mock_save, mock_load, expect, manager):
-            expect(manager.get_or_create(foo=1, bar=2)) == MyClass(foo=1, bar=2)
+            expect(manager.get_or_create(foo=1, bar=2, nested=None)) == MyClass(
+                foo=1, bar=2
+            )
             expect(mock_save.called).is_(True)
             expect(mock_load.called).is_(True)
 
@@ -66,4 +79,9 @@ def describe_manager():
         @patch('datafiles.mapper.Mapper.exists', False)
         def with_partial_positional_arguments(expect, manager):
             items = list(manager.filter(foo=1))
+            expect(items) == []
+
+        @patch('datafiles.mapper.Mapper.exists', False)
+        def with_nested_key_query(expect, manager):
+            items = list(manager.filter(nested__name='John Doe'))
             expect(items) == []
